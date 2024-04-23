a405726cfec7ea08ea6f503accc1037cfb478f82
123asdf123 <108726433+asdf123asdf123asdf123@users.noreply.github.com>
2024-04-23 18:50:33 +0800

    更加兼容

--- Auto_Sublime.bat
+++ Auto_Sublime.bat
@@ -2,5 +2,6 @@
 echo Do you need Mingw?(1/0)
 set /P need=
 if %need%==1 xcopy %cd%\mingw D:\mingw /S /I
+rmdir "%cd%\need\Sublime Text"
 xcopy "%cd%\need\Sublime Text" "C:\Users\%username%\AppData\Roaming\Sublime Text" /S /I
 pause
