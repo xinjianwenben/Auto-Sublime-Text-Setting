9d9ef6e8892c082daf06e41f5603bfc64d775106
123asdf123 <108726433+asdf123asdf123asdf123@users.noreply.github.com>
2024-07-01 09:11:11 +0800

    Update Auto_Sublime.cpp

--- Auto_Sublime.cpp
+++ Auto_Sublime.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 namespace estidi{
 	int in;
-    char cwd[256];
+	char cwd[256];
 	string q="";
 	void run(vector<string>vec,bool lei){
 		string s="";
@@ -17,10 +17,8 @@ namespace estidi{
 	DWORD cache=256;
 	string qu;
 	void debug(){cerr<<username<<' '<<q<<'\n';}
-	int main(int argc,char ** argv){
-		// freopen(".in","r",stdin);
-		// freopen(".out","w",stdout);
-        _getcwd(cwd,256);
+	int main(){
+		_getcwd(cwd,256);
 		for(int i=0;i<256;i++){
 			if(cwd[i]=='\0') break;
 			q+=cwd[i];
@@ -28,8 +26,6 @@ namespace estidi{
 		qu=q[0];
 		GetUserName(username,&cache);
 		string path=q+"\\need";
-//		if(argc>1)
-//			path=argv[1];
 		printf("Do you need Mingw?(1/0)");
 		scanf("%d",&in);
 		if(in) run({"C:\\Windows\\System32\\xcopy.exe \"",q,"\\mingw\" \"D:\\mingw\" /S /I"},0);
@@ -41,6 +37,6 @@ namespace estidi{
 		return 0;
 	}
 }
-int main(int argc,char ** argv){
-	return estidi::main(argc,argv);
+int main(){
+	return estidi::main();
 }
